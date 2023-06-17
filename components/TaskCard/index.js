8ad6027bf703f990fb53377fc1d1558c91f9e38b
Anastasiia Chetverykova <ana.chetverykova@gmail.com>
2023-06-17 13:56:44 +0200

    add task card styling

--- components/TaskCard/index.js
+++ components/TaskCard/index.js
@@ -6,20 +6,49 @@ const Article = styled.article`
   padding: 0.5rem;
 `;
 
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding-left: 0;
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  width: 100%;
+`;
+
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
 export default function TaskCard({
   title,
   category,
   deadline,
   prioritisation,
 }) {
+  const formattedDeadline = deadline
+    ? new Date(deadline).toLocaleDateString()
+    : "";
   return (
     <Article>
-      <ul>
-        <li>{title}</li>
-        <li>{category}</li>
-        <li>{deadline}</li>
-        <li>{prioritisation}</li>
-      </ul>
+      <List>
+        <ListItem>
+          title: <BoldText>{title}</BoldText>
+        </ListItem>
+        <ListItem>
+          category: <BoldText>{category}</BoldText>
+        </ListItem>
+        <ListItem>
+          deadline:<BoldText> {formattedDeadline}</BoldText>
+        </ListItem>
+        <ListItem>
+          priority:<BoldText> {prioritisation}</BoldText>
+        </ListItem>
+      </List>
     </Article>
   );
 }

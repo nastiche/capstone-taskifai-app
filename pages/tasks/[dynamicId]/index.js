4eb7233351e8d08306482fb513ae1f58bd3c9cea
Anastasiia Chetverykova <ana.chetverykova@gmail.com>
2023-06-22 11:44:06 +0200

    add back button to the task details page)

--- pages/tasks/[dynamicId]/index.js
+++ pages/tasks/[dynamicId]/index.js
@@ -1,27 +1,39 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import TaskDetails from "../../../components/TaskDetails";
+import Link from "next/link";
+import styled from "styled-components";
+
+const LinkWrapper = styled.div`
+  font-size: 3rem;
+  a {
+    text-decoration: none;
+  }
+`;
 
 export default function TaskDetailsPage() {
   const router = useRouter();
   const { isReady } = router;
   const { dynamicId } = router.query;
 
-  const {
-    data: task,
-    isLoading,
-    error,
-  } = useSWR(`/api/tasks/${dynamicId}`);
+  const { data: task, isLoading, error } = useSWR(`/api/tasks/${dynamicId}`);
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
 
   return (
-    <TaskDetails
-      title={task.title}
-      subtasks={task.subtasks}
-      tags={task.tags}
-      deadline={task.deadline}
-      priority={task.priority}
-    ></TaskDetails>
+    <>
+      <TaskDetails
+        title={task.title}
+        subtasks={task.subtasks}
+        tags={task.tags}
+        deadline={task.deadline}
+        priority={task.priority}
+      ></TaskDetails>
+      <LinkWrapper>
+        <Link href={`/`} passHref legacyBehavior>
+          🔙
+        </Link>
+      </LinkWrapper>
+    </>
   );
 }

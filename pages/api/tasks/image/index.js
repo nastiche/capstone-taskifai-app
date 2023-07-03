5e48ad5abb90ec5dd9e1c778cbed381092a2a9c0
Anastasiia Chetverykova <ana.chetverykova@gmail.com>
2023-07-03 17:05:50 +0200

    change error message im backend

--- pages/api/tasks/image/index.js
+++ pages/api/tasks/image/index.js
@@ -17,26 +17,25 @@ cloudinary.config({
 // define our async handler function - simplified :)
 export default async function handler(req, res) {
   // we check for POST, all other methods return 405
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-  // initialize formidable
-  const form = formidable({});
-  // using formidables' parse method to get a simple access to the file data
-  form.parse(req, async (error, fields, files) => {
-    // return an error status if parsing fails
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    // deconstruct our file from files data, will return an array with one element
-    const { file } = files;
-    // deconstruct the needed values from our file object at index 0
-    const { newFilename, filepath } = file[0];
-    // call our cloudinary uploader with the required arguments
-    const result = await cloudinary.v2.uploader.upload(filepath, {
-      public_id: newFilename,
+  if (req.method === "POST") {
+    // initialize formidable
+    const form = formidable({});
+    // using formidables' parse method to get a simple access to the file data
+    form.parse(req, async (error, fields, files) => {
+      // return an error status if parsing fails
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+      // deconstruct our file from files data, will return an array with one element
+      const { file } = files;
+      // deconstruct the needed values from our file object at index 0
+      const { newFilename, filepath } = file[0];
+      // call our cloudinary uploader with the required arguments
+      const result = await cloudinary.v2.uploader.upload(filepath, {
+        public_id: newFilename,
+      });
+      // return our just uploaded image result from cloudinary upload
+      return res.status(201).json(result);
     });
-    // return our just uploaded image result from cloudinary upload
-    return res.status(201).json(result);
-  });
+  }
 }
